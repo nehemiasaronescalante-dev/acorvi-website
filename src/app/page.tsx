1dbdf7f616a76a075e2dc13a28106a452055eed9
nehemiasaronescalante-dev <nehemiasaronescalante@gmail.com>
2026-04-17 03:19:27 -0500

    Update import paths for components in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import Navbar from "@/components/Navbar";
-import Hero from "../components/Hero";
-import Services from "../components/Services";
-import Process from "../components/Process";
-import Auditoria from "../components/Testimonials";
-import Contact from "../components/Contact";
-import Footer from "../components/Footer";
+import Hero from "@/components/Hero";
+import Services from "@/components/Services";
+import Process from "@/components/Process";
+import Auditoria from "@/components/Testimonials";
+import Contact from "@/components/Contact";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
